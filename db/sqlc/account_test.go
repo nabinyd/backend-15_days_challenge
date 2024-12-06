ad3f30b22defc7fa911b4cd065a9438ce45286b1
nabinyd <nabiny501@gmail.com>
2024-12-06 15:46:29 +0000

    updated mockgen for creating user

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -12,8 +12,9 @@ import (
 )
 
 func CreateRandomAccount(t *testing.T) Account {
+	user := CreateRandomUser(t)
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurreny(),
 	}
